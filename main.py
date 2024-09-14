6a4a867e502f167f76e14b5a38e5956b7312cb87
Андрей <elkabor@mail.ru>
2024-09-14 16:19:32 +0300

    downloaded data into excel

--- main.py
+++ main.py
@@ -5,46 +5,57 @@ from bs4 import BeautifulSoup
 import html5lib
 import yfinance as yf
 import openpyxl
+import string
 
-'''
-#parcing data
-names=[]
-startURL='https://finrange.com/ru/company/NASDAQ?page='
-for i in range(1,16):
-    URL=startURL+str(i)
-    response=requests.get(URL)
-    #print(response.status_code)
-    soup=BeautifulSoup(response.content,"html5lib")
-    #print(page)
-    items=soup.findAll('span','ticker')
-    for item in items:
-        names.append(item.text)
-print(len(names))
-#print(names)
-'''
 
-'''
-#writing data in file
-with open('names.txt', 'w'):
-    pass
-    
-with open('names.txt', 'w') as fp:
-    fp.write(' '.join(names))
-'''
+def get_names_from_url(URL):
+    names=[]
+    alphabet = list(string.ascii_uppercase)
+    for letter in alphabet:
+        new_URL=URL+letter+'.htm'
+        response=requests.get(new_URL)
+        print('response status = ',response.status_code)
+        soup=BeautifulSoup(response.content,"html5lib")
+        #print(page)
+        items=soup.findAll('tr','re')
+        for item in items:
+            names.append(item.td.text)
+        items = soup.findAll('tr', 'ro')
+        for item in items:
+            names.append(item.td.text)
+    print('number of stocks = ',len(names))
+    return names
 
-'''
-#reading data from file
-names=[]
-with open('names.txt') as file:
-    names= file.read().split(' ')
-#print(names)
+def save_names_to_file(names,file):
+    with open(file, 'w'):
+        pass
 
-#downloading data from yah00
-data=pd.DataFrame(columns=names)
-for name in names:
-    data[name]=yf.download(name,'2016-01-01','2016-12-31')['Adj Close']
+    with open(file, 'w') as fp:
+        fp.write(' '.join(names))
 
-#transfer data to input.txt
-data.to_excel('input.xlsx', index=False)
-'''
+def get_names_from_file(file):
+    with open(file) as fp:
+        names= fp.read().split(' ')
+    return names
+
+def download_stocks_to_excel(names,xlsx_file):
+    data=pd.DataFrame(columns=names)
+    for name in names:
+        data[name]=yf.download(name,'2016-01-01','2016-12-31')['Adj Close']
+
+    data.to_excel(xlsx_file, index=False)
+
+
+URL='https://www.eoddata.com/stocklist/NASDAQ/'
+names=get_names_from_url(URL)
+
+file='names.txt'
+save_names_to_file(names,file)
+names=get_names_from_file(file)
+
+xlsx_file='input.xlsx'
+download_stocks_to_excel(names,xlsx_file)
+#in xlsx f5,choose all blank and delete columns = 1902 stocks instead of 4817
+
+
 
